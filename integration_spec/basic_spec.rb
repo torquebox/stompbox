64f3c9454e651406a65a1ec582a814385f2c6a5d
Lance Ball <lball@redhat.com>
2011-11-18 16:49:16 -0500

    Make basic integ pass

--- integration_spec/basic_spec.rb
+++ integration_spec/basic_spec.rb
@@ -5,7 +5,9 @@ describe 'a basic test' do
   deploy <<-END.gsub( /^ {4}/, '' )
     application:
       root: #{File.dirname( __FILE__ )}/..
-    env: test
+    env: 
+      RACK_ENV: test
+      REQUIRE_AUTHENTICATION: false
     web:
       context: /stompbox-test
     ruby:
